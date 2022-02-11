f03cf8f1f8935be96b168fc54a2d3df87d928877
FerZamu <97319650+FerZamu@users.noreply.github.com>
2022-02-11 12:19:41 -0600

    Update load_products_dag.py

--- load_products_dag.py
+++ load_products_dag.py
@@ -42,7 +42,7 @@ class S3ToPostgresTransfer(BaseOperator):
         self.table = table
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
-        self.s3_key1 = s3_key1
+        #self.s3_key1 = s3_key1
         self.aws_conn_postgres_id  = aws_conn_postgres_id 
         self.aws_conn_id = aws_conn_id
         self.verify = verify
@@ -69,21 +69,21 @@ class S3ToPostgresTransfer(BaseOperator):
         # Validate if the file source exist or not in the bucket.
          
         if self.wildcard_match:
-            if not self.s3.check_for_wildcard_key(self.s3_key, self.s3_bucket) and not self.s3.check_for_wildcard_key(self.s3_key1, self.s3_bucket) :
+            if not self.s3.check_for_wildcard_key(self.s3_key, self.s3_bucket) #and not self.s3.check_for_wildcard_key(self.s3_key1, self.s3_bucket) :
                 raise AirflowException("No key matches {0}".format(self.s3_key,self.s3_key1))
             s3_key_object = self.s3.get_wildcard_key(self.s3_key, self.s3_bucket)
-            s3_key_object1 = self.s3.get_wildcard_key(self.s3_key1, self.s3_bucket)
+           # s3_key_object1 = self.s3.get_wildcard_key(self.s3_key1, self.s3_bucket)
         else:
-            if not self.s3.check_for_key(self.s3_key, self.s3_bucket)and not self.s3.check_for_wildcard_key(self.s3_key1, self.s3_bucket):
+            if not self.s3.check_for_key(self.s3_key, self.s3_bucket)and #not self.s3.check_for_wildcard_key(self.s3_key1, self.s3_bucket):
                 raise AirflowException(
                     "The key {0} does not exists".format(self.s3_key,self.s3_key1))
                   
             s3_key_object = self.s3.get_key(self.s3_key, self.s3_bucket)
-            s3_key_object1 = self.s3.get_key(self.s3_key1, self.s3_bucket)
+           # s3_key_object1 = self.s3.get_key(self.s3_key1, self.s3_bucket)
 
         # Read and decode the file into a list of strings.  
         list_srt_content = s3_key_object.get()['Body'].read().decode(encoding = "utf-8", errors = "ignore")
-        list_srt_content = s3_key_object1.get()['Body'].read().decode(encoding = "utf-8", errors = "ignore")
+       # list_srt_content = s3_key_object1.get()['Body'].read().decode(encoding = "utf-8", errors = "ignore")
         
         # schema definition for data types of the source.
         schema = {
@@ -196,7 +196,7 @@ load_movie_review = S3ToPostgresTransfer(
                             schema =  'bootcampdb', #'public'
                             table= 'movie_review',
                             s3_bucket = 's3-data-bootcampfz',
-                            s3_key1 = 'movie_review.csv',
+                         #   s3_key1 = 'movie_review.csv',
                             aws_conn_postgres_id = 'postgres_default',
                             aws_conn_id = 'aws_default'  
                           
